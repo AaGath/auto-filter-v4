0faef876e5b504213a4117dbcbb684f22efeb046
— Λsᴡɪɴ Rᴀᴊ — <82755354+Aswin-TG@users.noreply.github.com>
2021-07-24 14:37:09 +0530

    😶

--- values.py
+++ values.py
@@ -4,7 +4,7 @@ import time
 class Config(object):
 
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1786792404:AAGByiN85zeXhD5fpf_qUW_LiFl1Nkv5ltk")
 
 
     # Get from my.telegram.org (or @UseTGXBot)
@@ -35,13 +35,13 @@ class Config(object):
 
     # Go to https://dashboard.heroku.com/account, scroll down and press Reveal API
     # To check dyno status
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "")
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "bb6b34d0-a257-4eca-b622-1a772bfbaf7f")
 
 
     # OPTIONAL - To set alternate BOT COMMANDS
-    ADD_FILTER_CMD = os.environ.get("ADD_FILTER_CMD", "add")
-    DELETE_FILTER_CMD = os.environ.get("DELETE_FILTER_CMDD", "del")
-    DELETE_ALL_CMD = os.environ.get("DELETE_ALL_CMDD", "delall")
+    ADD_FILTER_CMD = os.environ.get("ADD_FILTER_CMD", "filter")
+    DELETE_FILTER_CMD = os.environ.get("DELETE_FILTER_CMDD", "delfilter")
+    DELETE_ALL_CMD = os.environ.get("DELETE_ALL_CMDD", "delallfilter")
     CONNECT_COMMAND = os.environ.get("CONNECT_COMMANDD", "connect")
     DISCONNECT_COMMAND = os.environ.get("DISCONNECT_COMMANDD", "disconnect")
 
