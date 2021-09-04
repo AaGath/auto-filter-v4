974fef7c99b6b9bde143bd739514ee0ac923d94c
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-09-04 21:44:06 +0530

    Update photos.py

--- bot/plugins/randomphotos/photos.py
+++ bot/plugins/randomphotos/photos.py
@@ -3,7 +3,9 @@ import threading
 from typing import Union
 
 PHOTOS = [
-    "link",
-    "Link",
-    "Link"
+    "https://telegra.ph/file/24145d0538190914ec841.jpg",
+    "https://telegra.ph/file/976a7a629af652cce8819.jpg",
+    "https://telegra.ph/file/60a52177ecd46b6078b9f.jpg",
+    "https://telegra.ph/file/95a95ccae435617f2579a.jpg",
+    "https://telegra.ph/file/f8376776c7ed85e1edc94.jpg"
 ]
